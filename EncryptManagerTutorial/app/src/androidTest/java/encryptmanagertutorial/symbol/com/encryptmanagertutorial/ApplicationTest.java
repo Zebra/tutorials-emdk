60d965797e3967d218038d5e8868c8db7fcfec77
rxfn78 <billie.hecox@motorolasolutions.com>
2016-04-20 10:28:11 -0500

    first commit

---
+++ EncryptManagerTutorial/app/src/androidTest/java/encryptmanagertutorial/symbol/com/encryptmanagertutorial/ApplicationTest.java
@@ -0,0 +1,13 @@
+package encryptmanagertutorial.symbol.com.encryptmanagertutorial;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
