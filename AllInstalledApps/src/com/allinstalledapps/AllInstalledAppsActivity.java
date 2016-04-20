3e902b50f5c69ff17c1bf880d4871b72339c30d6
rxfn78 <billie.hecox@motorolasolutions.com>
2016-04-20 09:31:24 -0500

    first commit

---
+++ AllInstalledApps/src/com/allinstalledapps/AllInstalledAppsActivity.java
@@ -0,0 +1,82 @@
+package com.allinstalledapps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class AllInstalledAppsActivity extends ListActivity {
+	private PackageManager packageManager = null;
+	private List<ApplicationInfo> applist = null;
+	private ApplicationAdapter listadaptor = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		packageManager = getPackageManager();
+		new LoadApplications().execute();
+
+	}
+
+	private List<ApplicationInfo> checkForLaunchIntent(
+			List<ApplicationInfo> list) {
+		ArrayList<ApplicationInfo> applist = new ArrayList<ApplicationInfo>();
+		for (ApplicationInfo info : list) {
+			try {
+				if (null != packageManager
+						.getLaunchIntentForPackage(info.packageName)) {
+					applist.add(info);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return applist;
+	}
+
+	private class LoadApplications extends AsyncTask<Void, Void, Void> {
+		private ProgressDialog progress = null;
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			applist = checkForLaunchIntent(packageManager
+					.getInstalledApplications(PackageManager.GET_META_DATA));
+			listadaptor = new ApplicationAdapter(AllInstalledAppsActivity.this,
+					R.layout.list_row, applist);
+
+			return null;
+		}
+
+		@Override
+		protected void onCancelled() {
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			setListAdapter(listadaptor);
+			progress.dismiss();
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			progress = ProgressDialog.show(AllInstalledAppsActivity.this, null,
+					"Loading application info...");
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			super.onProgressUpdate(values);
+		}
+	}
+}
