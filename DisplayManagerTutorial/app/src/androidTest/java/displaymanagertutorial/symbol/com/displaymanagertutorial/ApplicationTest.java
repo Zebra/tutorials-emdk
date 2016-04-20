934a917e282455dd8d6b2399094fdef50fcfe5ae
rxfn78 <billie.hecox@motorolasolutions.com>
2016-04-20 09:47:04 -0500

    first commit

---
+++ DisplayManagerTutorial/app/src/androidTest/java/displaymanagertutorial/symbol/com/displaymanagertutorial/ApplicationTest.java
@@ -0,0 +1,13 @@
+package displaymanagertutorial.symbol.com.displaymanagertutorial;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
